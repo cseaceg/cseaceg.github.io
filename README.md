78b01fad24838b895864c0afb6921cc97e1e7887
Md Faizaan <aulisius7@gmail.com>
2016-10-02 20:25:43 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+### Original Theme - Agency by Bootstrapious - http://bootstrapious.com
+
+Modified for use by CSEA, CEG.
+
+
+
---
+++ js/front.js
@@ -0,0 +1,73 @@
+$(function () {
+    sticky();
+    utils();
+});
+
+function sticky() {
+    $(".header").sticky();
+}
+
+function utils() {
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function (e) {
+        var radio = $(this).find(':radio');
+        radio.prop('checked', true);
+    });
+
+    $('.box.clickable').on('click', function (e) { window.location = $(this).find('a').attr('href')})
+
+    $('.external').on('click', function (e) {
+        e.preventDefault();
+        window.open($(this).attr("href"));
+    });
+
+    $('.scroll-to, #navigation a').click(function (event) {
+        event.preventDefault();
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+
+        $('body').scrollTo($('#' + trgt), 800, {offset: -50});
+    });
+}
+
+$.fn.alignElementsSameHeight = function () {
+    $('.same-height-row').each(function () {
+        var maxHeight = 0;
+        var children = $(this).find('.same-height');
+        children.height('auto');
+        if ($(window).width() > 768) {
+            children.each(function () {
+                if ($(this).innerHeight() > maxHeight) {
+                    maxHeight = $(this).innerHeight();
+                }
+            });
+            children.innerHeight(maxHeight);
+        }
+
+        maxHeight = 0;
+        children = $(this).find('.same-height-always');
+        children.height('auto');
+        children.each(function () {
+            if ($(this).innerHeight() > maxHeight) {
+                maxHeight = $(this).innerHeight();
+            }
+        });
+        children.innerHeight(maxHeight);
+    });
+}
+
+$(window).load(function () {
+    windowWidth = $(window).width();
+    $(this).alignElementsSameHeight();
+});
+
+$(window).resize(function () {
+    newWindowWidth = $(window).width();
+    if (windowWidth !== newWindowWidth) {
+        setTimeout(function () { $(this).alignElementsSameHeight()}, 100);
+        windowWidth = newWindowWidth;
+    }
+});
